79925b9e8977998b9986959bbdbcaee408461697
Theowlhooty <ssilvaabrantes@gmail.com>
2022-01-29 10:55:20 +0000

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-// Dado o HTML que tens na página (o botão), escreve JavaScript de modo a mudar os estilos para: background: green, padding: 0.2rem, color: white, border: solid 1px yellow, border-radius: 4px.
-
-// Seleciona o botão do DOM usando document.querySelector(). 
-let saveButton = ;
-
-// Muda os estilos usando ou Element.style ou Element.style.cssText
-
